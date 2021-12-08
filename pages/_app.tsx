bdb9f96eaa5ea74a77a3f8b2966e2a6b9f5963a4
Travis Weidenbenner <travis@weidenbenner.com>
2021-12-07 20:58:12 -0600

    Testing subdomains

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+  subdomain: string;
 }
 
-export default MyApp
+export default function MyApp({ Component, pageProps, subdomain }: MyAppProps) {
+  return (
+    <div>
+      <div>Subdomain {subdomain}</div>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  const { ctx } = appContext;
+  let subdomain: string = '';
+
+  if (ctx.req) {
+    subdomain = ctx.req.headers.host?.split('.')[0] ?? '';
+  }
+  
+  return { ...appProps, subdomain }
+}
